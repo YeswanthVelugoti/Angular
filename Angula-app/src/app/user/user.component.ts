ae0c773e227730b7390e6627d76a716872ab9f86
YeswanthVelugoti <115653951+YeswanthVelugoti@users.noreply.github.com>
2025-03-06 10:34:17 +0530

    Angular Application1
    I have used concepts :
    Components, Interpolation, Event binding, Property binding, Two-way binding, Built-in Directives, Reactive Forms

---
+++ Angula-app/src/app/user/user.component.ts
@@ -0,0 +1,53 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  imports: [ReactiveFormsModule,CommonModule,FormsModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  userForm: FormGroup;
+  submitted = false;
+
+  users: { name: string; email: string }[] = []; // Store registered users
+
+  // Two-way binding variables
+  nameInput: string = '';
+  emailInput: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+
+    this.submitted = true;
+
+    if (this.userForm.valid) {
+      // Save the user details
+      this.users.push({
+        name: this.userForm.value.name,
+        email: this.userForm.value.email,
+      });
+
+      alert('Signup Successful! 🎉');
+      this.userForm.reset();
+      this.submitted = false;
+
+      // Clear Two-Way Binding fields
+      this.nameInput = '';
+      this.emailInput = '';
+    }
+  }
+
+  get formControls() {
+    return this.userForm.controls;
+  }
+}
